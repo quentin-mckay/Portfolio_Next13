aa2186c3ce134871616b2b59ad9be1a5675dfee4
Quentin McKay <quentinmckay1@gmail.com>
2023-06-09 18:14:27 +1000

    added gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,25 @@ export const metadata = {
     description: 'Quentin McKay portfolio',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import Script from 'next/script'
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
     return (
         <html lang='en' className='bg-primary-bg text-white scroll-smooth'>
             <body>{children}</body>
+
+            {/* afterInteractive is the default strategy for Next's <Script> tag */}
+            <Script
+                async
+                strategy='afterInteractive'
+                src='https://www.googletagmanager.com/gtag/js?id=G-QGKSSLS9G4'
+            />
+            {/* <!-- Google tag (gtag.js) --> */}
+            <Script strategy='afterInteractive' src='/analytics.js' />
         </html>
     )
 }
